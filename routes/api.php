70dd7cce59d82b032346826962d1534fed6ac4b9
dusthoff <dustinh@onesi.de>
2017-08-26 14:25:23 +0200

    added some routes

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,21 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get("/project","ProjectController@index")->name("project.list");
+Route::post("/project","ProjectController@create")->name("project.create");
+Route::get("/project/{project}/repository","ProjectController@repository");
+Route::post("/project/{project}/repository","RepositoryController@create");
+Route::post("/project/{project?}/repository/{repository}/edit",function ($project,\App\Repository $repository, Request $request){
+    return App::make("App\Http\Controllers\RepositoryController")->update($repository,$request);
 });
+Route::get("/project/{project?}/repository/{repository}",function ($project,\App\Repository $repository){
+    return App::make("App\Http\Controllers\RepositoryController")->artifacts($repository);
+});
+Route::post("/project/{project?}/repository/{repository}",function ($project,\App\Repository $repository){
+    return App::make("App\Http\Controllers\RepositoryController")->upload($repository);
+});
+
+Route::get("/repository/{repository}","RepositoryController@artifacts")->name("repository");
+Route::get("/repository/{repository}/{artifact}","RepositoryController@version");
+Route::get("/repository/{repository}/{artifact}/{version}","RepositoryController@download");
+
